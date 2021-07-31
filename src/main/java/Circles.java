c751bffa8b4666e3510a5a138dff5ae56c3f506a
HarshaSudheer <harshasudheer@gmail.com>
2021-07-31 13:37:21 +0530

    four balls oop code

--- src/main/java/Circles.java
+++ src/main/java/Circles.java
@@ -1,3 +1,13 @@
 public class Circles {
-    
+    int xPos;
+    int height;
+    int speed;
+    public Circles(int height, int speed){
+        xPos=0;
+        this.height=height;
+        this.speed=speed;
+    }
+    public void updateXPos(){
+        xPos+=speed;
+    }
 }
--- src/main/java/TryProcessingOop.java
+++ src/main/java/TryProcessingOop.java
@@ -1,1 +1,30 @@
+import processing.core.PApplet;
 
+public class TryProcessingOop extends PApplet {
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+    public static final int DIAMETER = 10;
+    Circles circles1= new Circles(HEIGHT/5,1);
+    Circles circles2= new Circles(2*(HEIGHT/5),2);
+    Circles circles3= new Circles(3*(HEIGHT/5),3);
+    Circles circles4= new Circles(4*(HEIGHT/5),4);
+    public static void main(String[] args){
+        PApplet.main("TryProcessingOop",args);
+    }
+    @Override
+    public void settings(){
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+    @Override
+    public void draw(){
+        ellipse(circles1.xPos,circles1.height, DIAMETER,DIAMETER);
+        circles1.updateXPos();
+        ellipse(circles2.xPos,circles2.height,10,10);
+        circles2.updateXPos();
+        ellipse(circles3.xPos,circles3.height,10,10);
+        circles3.updateXPos();
+        ellipse(circles4.xPos,circles4.height,10,10);
+        circles4.updateXPos();
+    }
+}
